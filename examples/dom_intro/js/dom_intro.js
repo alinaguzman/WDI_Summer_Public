a57fd716d1101c8000be7fca10c15f0847751663
Alina Guzman <alinochka8@gmail.com>
2013-07-25 09:32:20 -0400

    i dont care about these changes but git is making me commit them!

--- examples/dom_intro/js/dom_intro.js
+++ examples/dom_intro/js/dom_intro.js
@@ -1,2 +1,15 @@
 // Traverse the DOM!
 
+function traverseDom(searchTerm){
+    var elements = document.getElementsByName(searchTerm);
+
+    for (var i = 0; i < elements.length; i++){
+        if (elements[i].innerText === searchTerm) {
+            return elements[i];
+        }
+    }
+return null;
+}
+
+traverseDom('i heart js');
+
